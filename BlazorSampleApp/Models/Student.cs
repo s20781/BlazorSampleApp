68ca86a7c8846d875e7af5a42a1f4de9bcd313b7
Franciszek Baron <s20781@pjwstk.edu.pl>
2022-06-03 01:18:24 +0200

    Blazor

--- BlazorSampleApp/Models/Student.cs
+++ BlazorSampleApp/Models/Student.cs
@@ -2,7 +2,11 @@
 {
     public class Student
     {
+        
+        public int Id { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
+
+        public string City { get; set; }
     }
 }
